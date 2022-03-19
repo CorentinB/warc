cf1c7df3a6ef07822d1f56329d3af5b266924371
Corentin Barreau <corentin@archive.org>
2022-03-20 00:45:16 +0100

    Remove unnecessary logging

--- client_test.go
+++ client_test.go
@@ -4,13 +4,9 @@ import (
 	"io"
 	"net/http"
 	"testing"
-
-	"github.com/sirupsen/logrus"
 )
 
 func TestWARCWritingHTTPClient(t *testing.T) {
-	logrus.Info("Initializing WARC writer..")
-
 	// init WARC rotator settings
 	var rotatorSettings = NewRotatorSettings()
 	var err error
@@ -18,15 +14,13 @@ func TestWARCWritingHTTPClient(t *testing.T) {
 	rotatorSettings.OutputDirectory = "warcs"
 	rotatorSettings.Compression = "GZIP"
 	rotatorSettings.Prefix = "TEST"
-	rotatorSettings.WarcinfoContent.Set("software", "Zeno")
 
 	// init the HTTP client responsible for recording HTTP(s) requests / responses
 	err = NewWARCWritingHTTPClient(rotatorSettings, "")
 	if err != nil {
-		logrus.Fatalf("Unable to init WARC writing HTTP client: %s", err)
+		t.Fatalf("Unable to init WARC writing HTTP client: %s", err)
 	}
 
-	logrus.Info("WARC writer initialized")
 	req, err := http.NewRequest("GET", "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAVeYY8.img?h=1536&w=2560&m=6&q=60&o=f&l=f&x=236&y=113", nil)
 
 	if err != nil {
