d9bfc4ef3f53fcf0cd082ec26d91a03336ea9e03
Corentin Barreau <corentin@archive.org>
2022-03-19 20:44:54 +0100

    WIP

--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package warc
 
 import (
+	"io"
 	"testing"
 
 	"github.com/sirupsen/logrus"
@@ -26,10 +27,13 @@ func TestWARCWritingHTTPClient(t *testing.T) {
 
 	logrus.Info("WARC writer initialized")
 
-	_, err = HTTPClient.Get("https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAVeYY8.img?h=1536&w=2560&m=6&q=60&o=f&l=f&x=236&y=113")
+	resp, err := HTTPClient.Get("https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAVeYY8.img?h=1536&w=2560&m=6&q=60&o=f&l=f&x=236&y=113")
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer resp.Body.Close()
+
+	io.Copy(io.Discard, resp.Body)
 
 	Close()
 }
--- go.mod
+++ go.mod
@@ -8,11 +8,9 @@ require (
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/slyrz/warc v0.0.0-20150806225202-a50edd19b690
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f
 )
 
 require (
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
