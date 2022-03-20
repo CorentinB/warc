0718da1032a4312706f5c4c3d474e480a170a3ad
Corentin Barreau <corentin@archive.org>
2022-03-20 02:25:06 +0100

    Fix: WARC headers case

--- header.go
+++ header.go
@@ -1,7 +1,5 @@
 package warc
 
-import "strings"
-
 // Header provides information about the WARC record. It stores WARC record
 // field names and their values. Since WARC field names are case-insensitive,
 // the Header methods are case-insensitive as well.
@@ -9,18 +7,18 @@ type Header map[string]string
 
 // Set sets the header field associated with key to value.
 func (h Header) Set(key, value string) {
-	h[strings.ToLower(key)] = value
+	h[key] = value
 }
 
 // Get returns the value associated with the given key.
 // If there is no value associated with the key, Get returns "".
 func (h Header) Get(key string) string {
-	return h[strings.ToLower(key)]
+	return h[key]
 }
 
 // Del deletes the value associated with key.
 func (h Header) Del(key string) {
-	delete(h, strings.ToLower(key))
+	delete(h, key)
 }
 
 // NewHeader creates a new WARC header.
