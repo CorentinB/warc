4d4e74e4a39a2acb881a549d65057bf419b28662
Corentin Barreau <corentin@archive.org>
2022-03-21 01:54:08 +0100

    Add: TestConcurrentWARCWritingWithHTTPClient

--- client_test.go
+++ client_test.go
@@ -7,9 +7,11 @@ import (
 	"net/http/httptest"
 	"path"
 	"testing"
+
+	"github.com/remeh/sizedwaitgroup"
 )
 
-func TestWARCWritingHTTPClient(t *testing.T) {
+func TestConcurrentWARCWritingWithHTTPClient(t *testing.T) {
 	// init test HTTP endpoint
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fileBytes, err := ioutil.ReadFile(path.Join("testdata", "image.svg"))
@@ -37,17 +39,65 @@ func TestWARCWritingHTTPClient(t *testing.T) {
 	}
 
 	req, err := http.NewRequest("GET", server.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	swg := sizedwaitgroup.New(8)
+
+	go func() {
+		defer swg.Done()
+
+		resp, err := HTTPClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		io.Copy(io.Discard, resp.Body)
+	}()
+
+	swg.Wait()
+
+	Close()
+}
+
+func TestWARCWritingWithHTTPClient(t *testing.T) {
+	// init test HTTP endpoint
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fileBytes, err := ioutil.ReadFile(path.Join("testdata", "image.svg"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "image/svg+xml")
+		w.Write(fileBytes)
+	}))
+
+	// init WARC rotator settings
+	var rotatorSettings = NewRotatorSettings()
+	var err error
+
+	rotatorSettings.OutputDirectory = "warcs"
+	rotatorSettings.Compression = "GZIP"
+	rotatorSettings.Prefix = "TEST"
+
+	// init the HTTP client responsible for recording HTTP(s) requests / responses
+	err = NewWARCWritingHTTPClient(rotatorSettings, "")
+	if err != nil {
+		t.Fatalf("Unable to init WARC writing HTTP client: %s", err)
+	}
+
+	req, err := http.NewRequest("GET", server.URL, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	resp, err := HTTPClient.Do(req)
-
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	defer resp.Body.Close()
 
 	io.Copy(io.Discard, resp.Body)
--- go.mod
+++ go.mod
@@ -4,12 +4,9 @@ go 1.18
 
 require (
 	github.com/klauspost/compress v1.15.1
+	github.com/remeh/sizedwaitgroup v1.0.0
 	github.com/satori/go.uuid v1.2.0
-	github.com/sirupsen/logrus v1.8.1
 	github.com/slyrz/warc v0.0.0-20150806225202-a50edd19b690
 )
 
-require (
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-)
+require gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
