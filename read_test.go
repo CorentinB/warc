cfc9f69766fbaf778401bdfcba08918baefdf51c
Jake L <NGTmeaty@users.noreply.github.com>
2022-06-20 13:32:01 -0400

    Some minor testing improvements (#10)
    * feat: rebase changes from older branch, refactor to allow reading from multiple WARC files.
    
    * feat: add back concurrency testing with larger file
    
    * feat: check WARC-Block-Digest on all of our current HTTP Client write tests.
    
    * fix: utilize unused "testFileScan" (even though it's not very useful!)
    
    remove some unused lines.
    general code cleanup, older tests seem .. not that useful, imo.
    
    * Fix: spelling issues, other general stuff.
    
    * Fix: wording
    
    * fix: address review feedback

--- read_test.go
+++ read_test.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"testing"
 )
 
 func testFileHash(t *testing.T, path string) {
+	t.Logf("checking 'WARC-Block-Digest' on %q", path)
+
 	file, err := os.Open(path)
 	if err != nil {
 		t.Fatalf("failed to open %q: %v", path, err)
@@ -54,33 +57,45 @@ func testFileScan(t *testing.T, path string) {
 		}
 		total++
 	}
-	if total != 10 {
-		t.Fatalf("expected 50 records, got %v", total)
+
+	if total != 3 {
+		t.Fatalf("expected 3 records, got %v", total)
 	}
 }
 
-func testFileSingleHashCheck(t *testing.T, path string, hash string, expectedTotal int) {
+func testFileSingleHashCheck(t *testing.T, path string, hash string, expectedTotal int) int {
+	// The below function validates the Block-Digest per record while the function we are in checks for a specific Payload-Digest in records :)
+	testFileHash(t, path)
+
 	file, err := os.Open(path)
 	if err != nil {
 		t.Fatalf("failed to open %q: %v", path, err)
 	}
 	defer file.Close()
 
+	t.Logf("checking 'WARC-Payload-Digest' on %q", path)
+
 	reader, err := NewReader(file)
 	if err != nil {
 		t.Fatalf("warc.NewReader failed for %q: %v", path, err)
 	}
 
-	total_read := 0
+	totalRead := 0
 
 	for {
 		record, err := reader.ReadRecord()
 		if err == io.EOF {
-			if total_read == expectedTotal {
+			if expectedTotal == -1 {
+				// This is expected for multiple file WARCs as we need to count the total count outside of this function.
+				return totalRead
+			}
+
+			if totalRead == expectedTotal {
 				// We've read the expected amount and reached the end of the WARC file. Time to break out.
 				break
 			} else {
-				t.Fatalf("warc: unexpected number of records read")
+				t.Fatalf("warc: unexpected number of records read. read: " + strconv.Itoa(totalRead) + " expected: " + strconv.Itoa(expectedTotal))
+				return -1
 			}
 		}
 
@@ -98,110 +113,17 @@ func testFileSingleHashCheck(t *testing.T, path string, hash string, expectedTot
 			t.Fatalf("WARC-Payload-Digest doesn't match intended result %s != %s", record.Header.Get("WARC-Payload-Digest"), hash)
 		}
 
-		total_read++
+		totalRead++
 	}
+	return -1
 }
 
 func TestReader(t *testing.T) {
 	var paths = []string{
 		"testdata/test.warc.gz",
 	}
-
 	for _, path := range paths {
 		testFileHash(t, path)
+		testFileScan(t, path)
 	}
 }
-
-var testRecords = []struct {
-	Header  map[string]string
-	Content []byte
-}{
-	{
-		Header: map[string]string{
-			"foo": "bar",
-			"baz": "qux",
-		},
-		Content: []byte("Hello, World!"),
-	},
-	{
-		Header: map[string]string{
-			"some-key":    "some value",
-			"another-key": "another value",
-		},
-		Content: []byte("Multiline\nText\n"),
-	},
-	{
-		Header: map[string]string{
-			"key 1": "value 1",
-			"key 2": "value 2",
-			"key 3": "value 3",
-			"key 4": "value 4",
-			"key 5": "value 5",
-		},
-		Content: []byte{
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		},
-	},
-}
-
-// func TestSimpleWriteRead(t *testing.T) {
-// 	buffer := new(bytes.Buffer)
-
-// 	// We write the test records to the warc writer
-// 	writer, err := NewWriter(buffer, "test.warc.gz", "GZIP")
-// 	if err != nil {
-// 		t.Fatalf("failed to initialize a new writer: %v", err)
-// 	}
-
-// 	for i, testRecord := range testRecords {
-// 		t.Logf("writing record %d", i)
-// 		record := NewRecord()
-// 		record.Header = testRecord.Header
-// 		record.Content = bytes.NewReader(testRecord.Content)
-
-// 		_, err := writer.WriteRecord(record)
-// 		if err != nil {
-// 			t.Fatalf("error while writing test record: %v", err)
-// 		}
-
-// 		writer.GZIPWriter.Close()
-// 		writer, err = NewWriter(buffer, "test.warc.gz", "GZIP")
-// 		if err != nil {
-// 			t.Fatalf("failed to initialize a new writer: %v", err)
-// 		}
-// 	}
-
-// 	// Now we try to read the records of the previously written
-// 	// warc writer, and test if we get the expected result
-// 	reader, err := NewReader(buffer)
-// 	if err != nil {
-// 		t.Fatalf("failed to create reader: %v", err)
-// 	}
-// 	defer reader.Close()
-
-// 	// We read the records and test if we get the expected output
-// 	for i, testRecord := range testRecords {
-// 		t.Logf("reading record %d", i)
-// 		record, err := reader.ReadRecord(false)
-// 		if err != nil {
-// 			t.Fatalf("expected record, got %v", err)
-// 		}
-
-// 		// Test the headers
-// 		for key, val := range testRecord.Header {
-// 			if record.Header[key] != val {
-// 				t.Errorf("expected %q = %q, got %q", key, val, record.Header[key])
-// 			}
-// 		}
-
-// 		// Test the record content
-// 		content, err := ioutil.ReadAll(record.Content)
-// 		if err != nil {
-// 			t.Fatalf("failed reading the test record: %v", err)
-// 		}
-
-// 		if string(content) != string(testRecord.Content) {
-// 			t.Errorf("expected %s = %s", content, testRecord.Content)
-// 		}
-// 	}
-// }
