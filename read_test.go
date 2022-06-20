e8efe471ec2c8ce51db00dd5ef10a639f230bb73
Jake L <NGTmeaty@users.noreply.github.com>
2022-06-20 01:15:50 +0000

    fix: utilize unused "testFileScan" (even though it's not very useful!)
    remove some unused lines.
    general code cleanup, older tests seem .. not that useful, imo.

--- read_test.go
+++ read_test.go
@@ -57,8 +57,8 @@ func testFileScan(t *testing.T, path string) {
 		}
 		total++
 	}
-	if total != 10 {
-		t.Fatalf("expected 50 records, got %v", total)
+	if total != 3 {
+		t.Fatalf("expected 3 records, got %v", total)
 	}
 }
 
@@ -125,99 +125,7 @@ func TestReader(t *testing.T) {
 
 	for _, path := range paths {
 		testFileHash(t, path)
+		testFileScan(t, path)
 	}
 }
 
-var testRecords = []struct {
-	Header  map[string]string
-	Content []byte
-}{
-	{
-		Header: map[string]string{
-			"foo": "bar",
-			"baz": "qux",
-		},
-		Content: []byte("Hello, World!"),
-	},
-	{
-		Header: map[string]string{
-			"some-key":    "some value",
-			"another-key": "another value",
-		},
-		Content: []byte("Multiline\nText\n"),
-	},
-	{
-		Header: map[string]string{
-			"key 1": "value 1",
-			"key 2": "value 2",
-			"key 3": "value 3",
-			"key 4": "value 4",
-			"key 5": "value 5",
-		},
-		Content: []byte{
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-		},
-	},
-}
-
-// func TestSimpleWriteRead(t *testing.T) {
-// 	buffer := new(bytes.Buffer)
-
-// 	// We write the test records to the warc writer
-// 	writer, err := NewWriter(buffer, "test.warc.gz", "GZIP")
-// 	if err != nil {
-// 		t.Fatalf("failed to initialize a new writer: %v", err)
-// 	}
-
-// 	for i, testRecord := range testRecords {
-// 		t.Logf("writing record %d", i)
-// 		record := NewRecord()
-// 		record.Header = testRecord.Header
-// 		record.Content = bytes.NewReader(testRecord.Content)
-
-// 		_, err := writer.WriteRecord(record)
-// 		if err != nil {
-// 			t.Fatalf("error while writing test record: %v", err)
-// 		}
-
-// 		writer.GZIPWriter.Close()
-// 		writer, err = NewWriter(buffer, "test.warc.gz", "GZIP")
-// 		if err != nil {
-// 			t.Fatalf("failed to initialize a new writer: %v", err)
-// 		}
-// 	}
-
-// 	// Now we try to read the records of the previously written
-// 	// warc writer, and test if we get the expected result
-// 	reader, err := NewReader(buffer)
-// 	if err != nil {
-// 		t.Fatalf("failed to create reader: %v", err)
-// 	}
-// 	defer reader.Close()
-
-// 	// We read the records and test if we get the expected output
-// 	for i, testRecord := range testRecords {
-// 		t.Logf("reading record %d", i)
-// 		record, err := reader.ReadRecord(false)
-// 		if err != nil {
-// 			t.Fatalf("expected record, got %v", err)
-// 		}
-
-// 		// Test the headers
-// 		for key, val := range testRecord.Header {
-// 			if record.Header[key] != val {
-// 				t.Errorf("expected %q = %q, got %q", key, val, record.Header[key])
-// 			}
-// 		}
-
-// 		// Test the record content
-// 		content, err := ioutil.ReadAll(record.Content)
-// 		if err != nil {
-// 			t.Fatalf("failed reading the test record: %v", err)
-// 		}
-
-// 		if string(content) != string(testRecord.Content) {
-// 			t.Errorf("expected %s = %s", content, testRecord.Content)
-// 		}
-// 	}
-// }
