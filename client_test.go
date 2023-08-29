0c1f72498552186899d74163754955a9f99bac20
Jake LaFountain <jakelf@archive.org>
2023-08-29 06:24:30 -0400

    Create new non-chunked test.
    Verified through Heritrix.
    
    software: Heritrix/3.4.0-hq1-SNAPSHOT-2022-05-30T18:40:31Z
    ---
    WARC/1.0
    WARC-Type: response
    WARC-Target-URI: http://127.0.0.1:35409/
    WARC-Date: 2023-08-29T10:18:25Z
    WARC-IP-Address: 127.0.0.1
    WARC-Payload-Digest: sha1:3TOI6NZK7GYJSFYGATOMMNM2C5VPT3ZD
    WARC-Record-ID: <urn:uuid:14d61a70-18a2-42dc-890a-e9d04e61abc5>
    Content-Type: application/http; msgtype=response
    Content-Length: 161

--- client_test.go
+++ client_test.go
@@ -1011,6 +1011,67 @@ func TestHTTPClientWithoutIoCopy(t *testing.T) {
 	}
 }
 
+func TestHTTPClientWithoutChunkEncoding(t *testing.T) {
+	var (
+		rotatorSettings = NewRotatorSettings()
+		errWg           sync.WaitGroup
+		err             error
+	)
+
+	// init test HTTP endpoint
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("small text string to ensure it isn't chunked"))
+	}))
+	defer server.Close()
+
+	rotatorSettings.OutputDirectory, err = ioutil.TempDir("", "warc-tests-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// defer os.RemoveAll(rotatorSettings.OutputDirectory)
+
+	rotatorSettings.Prefix = "TEST"
+
+	// init the HTTP client responsible for recording HTTP(s) requests / responses
+	httpClient, err := NewWARCWritingHTTPClient(HTTPClientSettings{RotatorSettings: rotatorSettings})
+	if err != nil {
+		t.Fatalf("Unable to init WARC writing HTTP client: %s", err)
+	}
+
+	errWg.Add(1)
+	go func() {
+		defer errWg.Done()
+		for err := range httpClient.ErrChan {
+			t.Errorf("Error writing to WARC: %s", err.Err.Error())
+		}
+	}()
+
+	req, err := http.NewRequest("GET", server.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(io.Discard, resp.Body)
+
+	httpClient.Close()
+
+	files, err := filepath.Glob(rotatorSettings.OutputDirectory + "/*")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, path := range files {
+		testFileSingleHashCheck(t, path, "sha1:3TOI6NZK7GYJSFYGATOMMNM2C5VPT3ZD", []string{"180"}, 1)
+	}
+}
+
 func BenchmarkConcurrentUnder2MB(b *testing.B) {
 	var (
 		rotatorSettings = NewRotatorSettings()
