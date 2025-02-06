8b273a877d407e40f531712e17ed47b0046e01a0
Thomas FOUBERT <perso@thomas-foubert.com>
2025-02-06 17:58:54 +0100

    feat: make the dialer more flexible with TCP keepalives

--- dialer.go
+++ dialer.go
@@ -45,9 +45,9 @@ func newCustomDialer(httpClient *CustomHTTPClient, proxyURL string, DialTimeout,
 	d.disableIPv6 = disableIPv6
 
 	d.KeepAliveConfig.Enable = true
-	d.KeepAliveConfig.Idle = 15 * time.Second
-	d.KeepAliveConfig.Interval = 15 * time.Second
-	d.KeepAliveConfig.Count = 9
+	d.KeepAliveConfig.Idle = 30 * time.Second
+	d.KeepAliveConfig.Interval = 30 * time.Second
+	d.KeepAliveConfig.Count = 3
 
 	d.DNSRecordsTTL = DNSRecordsTTL
 	d.DNSRecords = new(sync.Map)
